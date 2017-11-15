ab7372b97f83440129574f468465ffebe37831f5
AnaLajas <33604938+AnaLajas@users.noreply.github.com>
2017-11-15 17:59:01 +0100

    mensajes si no puedes entrar

--- comprobarusuario.php
+++ comprobarusuario.php
@@ -10,43 +10,49 @@
 include "tablas.php";
 
 //FUNCIÓN:
-
 //Si el usuario existe (es decir, coincide con $usuario):
 function comprobarDatos($usuario,$clave,$USUARIOS){
 	$usuarioEncontrado = false;	
 	for($indice=0; $indice<count($USUARIOS); $indice++){
 		if ($usuario == $USUARIOS[$indice]["usuario"]){
-			echo "<p>Usuario registrado</p>";
+			/* contenedor */
+			echo "<div style='text-align:center; background-color:#D5DBDB; margin: 200px auto;width:300px; height: 120px; border: 2px solid black; border-radius:6px;'>";
+			echo "<h3 style='color:#17202A;'>Usuario registrado</h3>";
 			$usuarioEncontrado = true;
 			if ($clave != $USUARIOS[$indice]["clave"]){
-				echo"<p>La contraseña introducida es incorrecta</p>";
-				//AÑADIR UN ENLACE PARA VOLVER AL FORMULARIO
-				echo "<a href='login.html'>". "Volver". "</a>";
+				/* contenedor */ 
+				echo"<h3 style='color:#A93226;'>¡La contraseña introducida es INCORRECTA!</h3>";
+				echo "</div>";
+				/* enlace para volver al login */
+				echo "<div style='text-align:center;'>";
+				echo "<a href= 'login.html'><button style='padding: 4px; border-radius: 3px; background-color: #F2F3F4;'>Volver</button></a>";
+				echo"</div>";
 			} else {
 				echo"<p>Adelante, los datos introducidos son correctos</p>";
 				//ahora iniciaría sesión y le llevaríamos a otra página (principal):
 				//iniciamos la sesión:
 				session_start();
 				$_SESSION['nombre']=$usuario;
-				/*crear una cookie con la fecha*/
+				//crear una cookie con la fecha
 			    setcookie("fecha", date("d/m/Y H:i:s"), time()+3600);
 				header('Location: principal.php');
 			}
 		}
 	}
 	if ($usuarioEncontrado == false) {
-		echo"<p>No estás registrado como usuario</p>";
-		//AÑADIR UN ENLACE PARA VOLVER AL FORMULARIO
-		echo "<a href='login.html'>"."Volver"."</a>";
+		/* contenedor */
+		echo "<div style='text-align:center; background-color:#D5DBDB; margin: 200px auto;width:300px; height: 77px; border: 2px solid black; border-radius:6px;'>";		
+		echo"<h3 style='color:#CD6155;'>¡NO ESTÁ REGISTRADO COMO USUARIO!</h3>";
+		echo "</div>";
+		/* enlace para volver al login */
+		echo "<div style='text-align:center;'>";
+		echo "<a href= 'login.html'><button style='padding: 4px; border-radius: 3px; background-color: #F2F3F4;'>Volver</button></a>";
+		echo"</div>";
 	}	
 }
-
-//Comprobamos que los datos del formulario sean correctos:
+// Comprobamos que los datos del formulario sean correctos:
 $usuario = $_POST['usuario'];
 $pass = $_POST['clave'];
-
-
-//Llamamos a la función:		
+// Llamamos a la función:		
 comprobarDatos ($usuario,$pass,$USUARIOS);
-
 ?>
--- tablas.php
+++ tablas.php
@@ -2,8 +2,8 @@
 /*crear array bidimensional (un array dentro de otro array)*/
 /*  tabla alumnos */
     $ALUMNOS = array(
-        "001" => array( "nombre" => "Jesús", "curso" => 1, "notaMates" => 8, "notaFisica" => 6),
-        "002" => array( "nombre" => "Ana", "curso" => 2, "notaMates" => 9, "notaFisica" => 6)
+        0 => array( "nombre" => "Jesús", "curso" => 1, "notaMates" => 8, "notaFisica" => 6),
+        1 => array( "nombre" => "Ana", "curso" => 2, "notaMates" => 9, "notaFisica" => 6)
         );
 /* tabla usuarios */
     $USUARIOS = array (
